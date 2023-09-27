e00d7a72c8cd07eff05a0dbcde8bfcf37bfb297b
Amiel Antonio <amielnicole.antonio@gmail.com>
2023-09-28 07:17:10 +0800

    add favicon package

--- functions.php
+++ functions.php
@@ -85,3 +85,13 @@ function model_custom_logo() {
 
 	echo $html;
 }
+
+add_action('wp_head', function() {
+	?>
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri() . "/favicon_package" ?>/favicon-16x16.png">
+	<link rel="manifest" href="<?php echo get_stylesheet_directory_uri() . "/favicon_package" ?>/site.webmanifest">
+	<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri() . "/favicon_package" ?>/safari-pinned-tab.svg" color="#cb6959">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+<?php
+});
